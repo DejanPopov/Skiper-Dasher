2d9561d37fec9fbd3e45b173e64c70179ccbde6a
Dejan <dekipopica92@gmail.com>
2025-01-20 11:59:32 +0100

    Drawing blue rectangle that will go up if SPACE key is pressed

--- game_files/dasher.cpp
+++ game_files/dasher.cpp
@@ -4,21 +4,38 @@ int main()
 {    
     // Window Dimensions
     // const - Value can't be changed!
-    const int windowWith   {512};
+    const int windowWidth   {512};
     const int windowHeight {380};
     
     // Widnow Initialisation and FPS
     InitWindow(windowWidth, windowHeight, "Skiper Dasher!");
     SetTargetFPS(60);
 
+    // Rectangle diimesions
+    const int  rectangleWidth  {50};
+    const int  rectangleHeight {80};
+
+    if(isKeyPressed(KEY_SPACE))
+    {
+        velocity -= 10;
+    }
+
+    int posY     {windowHeight - rectangleHeight};
+    int velocity {-10};
+
+    
     while (!WindowShouldClose())
     { 
         // Start drawing
         BeginDrawing();
         ClearBackground(WHITE);
 
+        posY += velocity;
+        DrawRectangle(windowWidth/2, posY, ,rectangleWidth rectangleHeight, BLUE);
+
         // Stop Drawing
         EndDrawing();
     }
+    CloseWindow();
 
 }
