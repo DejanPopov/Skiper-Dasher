8194d1dd9be69eedf492acff414e4c6962de5742
Dejan <dekipopica92@gmail.com>
2025-01-20 11:28:49 +0100

    init commit from Raider IDE

---
+++ game_files/dasher.cpp
@@ -0,0 +1,12 @@
+#include "raylib.h"
+
+int main()
+{
+    // Window Dimensions
+    // const - Value can't be changed!
+    const int windowWith   {512};
+    const int windowHeight {380};
+    
+    SetTargetFPS(60);
+
+}
