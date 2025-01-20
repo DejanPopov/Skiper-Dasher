ce89e507a47b828d3d011af7b6e986bb4bf59a5c
Dejan <dekipopica92@gmail.com>
2025-01-20 11:41:56 +0100

    How to draw a window with basic background color

--- game_files/dasher.cpp
+++ game_files/dasher.cpp
@@ -1,12 +1,24 @@
 #include "raylib.h"
 
 int main()
-{
+{    
     // Window Dimensions
     // const - Value can't be changed!
     const int windowWith   {512};
     const int windowHeight {380};
     
+    // Widnow Initialisation and FPS
+    InitWindow(windowWidth, windowHeight, "Skiper Dasher!");
     SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    { 
+        // Start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Stop Drawing
+        EndDrawing();
+    }
 
 }
